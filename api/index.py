8d9cb12353ba0315fb3f67538fa8600cb4283618
ninest <parth.kabra@gmail.com>
2020-08-01 02:36:57 +0800

    create api

---
+++ api/index.py
@@ -0,0 +1,23 @@
+from http.server import BaseHTTPRequestHandler
+import json
+from datetime import datetime
+
+from enlist.get import get
+from enlist.values import CURRENT_YEAR
+
+
+class handler(BaseHTTPRequestHandler):
+
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header('Content-type', 'application/json')
+    self.end_headers()
+
+    events_list = get(in_json=True)
+
+    resp = {
+        'year': CURRENT_YEAR,
+        'events_list': events_list
+    }
+    self.wfile.write(json.dumps(resp).encode())
+    return
--- enlist/get.py
+++ enlist/get.py
@@ -1,5 +1,6 @@
 import json
 from .models.event import Event
+from .values import CURRENT_YEAR
 
 # https://www.cmpb.gov.sg/web/wcm/connect/cmpb/cmpbContent/CMPBHome/before-ns/Enlistment-dates/enlistment-dates?srv=cmpnt&source=library&cmpntname=CMPBDesign/sections/page-2-BeforeNS/enlistment-dates/NAV%20Calendar%20Events%20Json"
 
@@ -11,6 +12,8 @@ def get(in_json=False):
   event_list = []
   for event_dict in data["calendarEventList"][1:]:
     event = Event.from_json(event_dict)
-    event_list.append(event.to_json() if in_json else event)
+
+    if event.start.year == CURRENT_YEAR:
+      event_list.append(event.to_json() if in_json else event)
 
   return event_list
